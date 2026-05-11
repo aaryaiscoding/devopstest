7292de89d8e63b5bedc7f12af1c3b1ac1f965661
Aarya <139015471+aaryaiscoding@users.noreply.github.com>
2026-05-11 12:28:00 +0530

    Update App.java

--- src/main/java/com/selenium/App.java
+++ src/main/java/com/selenium/App.java
@@ -1,13 +1,17 @@
 package com.selenium;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Google Opened");
+
+        // driver.quit(); // optional
     }
 }
